7d2916fa525818cba8e4b28074263036cc634d9d
kazuki1023 <k.keio12569@gmail.com>
2023-09-06 12:09:12 +0900

    add navbarにログインcomponent

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Navbar.css";
+import LoginButton from "./login";
 
 const Navbar = ({pockemonCount}) => {
   return (
     <nav>
       <h1>ポケモン図鑑</h1>
       <p>表示されてるポケモン:{pockemonCount}</p>
+      <LoginButton />
     </nav>
   );
 };
